647d14d5fe9e843489cffacd169e3f274b063909
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2021-12-29 15:36:09 -0300

    feat: sidebar footer updated

--- src/renderer/components/SidebarFooter.tsx
+++ src/renderer/components/SidebarFooter.tsx
@@ -1,19 +1,12 @@
-import { FiInfo, FiMoon, FiSliders } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
 import '../assets/styles/components/SidebarFooter.scss';
+import { version } from '../../../release/app/package.json';
 
 export default function SidebarFooter(): JSX.Element {
   return (
     <footer id="sidebarFooter">
-      <Link to="/about">
-        <FiInfo />
-      </Link>
-      <button type="button">
-        <FiMoon />
-      </button>
-      <Link to="/settings">
-        <FiSliders />
-      </Link>
+      <span>
+        <b>Fluig Monitor</b> - v{version}
+      </span>
     </footer>
   );
 }
