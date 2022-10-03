f8db27114d74a38bab4a98d2f0d00428dc6a17b8
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2022-10-03 13:59:30 -0300

    feat: start:clean script clears all log files

--- scripts/clearDevLogs.ts
+++ scripts/clearDevLogs.ts
@@ -4,16 +4,24 @@ import * as fs from 'fs';
 
 const appData = process.env.APPDATA;
 
-// TODO: Clear all log files on the log folder
 console.log('🧹 Starting log file cleanup');
 if (appData) {
-  const logPath = path.resolve(appData, 'fluig-monitor', 'logs', 'app.dev.log');
+  let clearedFiles = 0;
+  const logPath = path.resolve(appData, 'fluig-monitor', 'logs');
   console.log(`📂 Log file location: ${logPath}`);
 
   if (fs.existsSync(logPath)) {
-    fs.writeFile(logPath, '', () => {
-      console.log('✅ Log file cleared');
+    fs.readdirSync(logPath).forEach((file) => {
+      fs.rmSync(path.resolve(logPath, file));
+
+      clearedFiles += 1;
     });
+
+    if (clearedFiles > 0) {
+      console.log(`✅ ${clearedFiles} log files have been deleted`);
+    } else {
+      console.log(`✅ There are no log files to be deleted`);
+    }
   }
 } else {
   console.log('❌ AppData folder could not be found');
