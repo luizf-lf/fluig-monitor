f0cd719705941bedffd8927592f6c46c52823fcd
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2022-10-06 09:15:15 -0300

    feat: updated log clear script

--- scripts/clearDevLogs.ts
+++ scripts/clearDevLogs.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import path from 'path';
 import * as fs from 'fs';
+import formatBytes from '../src/main/utils/formatBytes';
 
 const appData = process.env.APPDATA;
 
@@ -9,16 +10,24 @@ if (appData) {
   let clearedFiles = 0;
   const logPath = path.resolve(appData, 'fluig-monitor', 'logs');
   console.log(`📂 Log file location: ${logPath}`);
+  let totalSize = 0;
 
   if (fs.existsSync(logPath)) {
     fs.readdirSync(logPath).forEach((file) => {
+      const stats = fs.statSync(path.resolve(logPath, file));
       fs.rmSync(path.resolve(logPath, file));
 
+      totalSize += stats.size;
       clearedFiles += 1;
     });
 
     if (clearedFiles > 0) {
       console.log(`✅ ${clearedFiles} log files have been deleted`);
+      console.log(
+        `🌌 A total of ${formatBytes(
+          totalSize
+        )} have been purged from this plane of reality`
+      );
     } else {
       console.log(`✅ There are no log files to be deleted`);
     }
