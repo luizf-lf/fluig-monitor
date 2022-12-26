ce5dd0d45a95efabe12d72da81117427c355010a
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2022-12-26 17:05:03 -0300

    feat: theme settings component updated

--- src/renderer/components/SettingsView/ThemeSettings.tsx
+++ src/renderer/components/SettingsView/ThemeSettings.tsx
@@ -1,7 +1,14 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import { motion } from 'framer-motion';
+import { FiPenTool } from 'react-icons/fi';
 import globalContainerVariants from '../../utils/globalContainerVariants';
 
 export default function ThemeSettings() {
+  const theme = document.body.classList.contains('dark-theme')
+    ? 'DARK'
+    : 'WHITE';
+
+  // TODO: Update with preview selector (resources at renderer/assets/img/)
   return (
     <motion.div
       variants={globalContainerVariants}
@@ -9,7 +16,24 @@ export default function ThemeSettings() {
       animate="visible"
       exit="exit"
     >
-      ThemeSettings
+      <h3>
+        <FiPenTool /> Tema
+      </h3>
+      <p>Selecione abaixo o tema que deverá ser utilizado pela aplicação:</p>
+
+      <div className="form-group">
+        <select
+          name="appTheme"
+          id="appTheme"
+          defaultValue={theme}
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        >
+          <option value="WHITE">Claro</option>
+          <option value="DARK">Escuro</option>
+        </select>
+      </div>
     </motion.div>
   );
 }
