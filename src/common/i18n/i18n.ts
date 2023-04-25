4406a4d3bc5c1dbc9e69ea02a05dbe3425d083ca
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2023-04-25 14:29:56 -0300

    feat: updated language files to json

--- src/common/i18n/i18n.ts
+++ src/common/i18n/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import detector from 'i18next-browser-languagedetector';
-import pt from './resources/pt';
-import en from './resources/en';
+import pt from './resources/pt.json';
+import en from './resources/en.json';
 
 // using resources exported from local files since the 'i18next-fs-backend' package doesn't work properly.
 //  it also solves the production build "error".
